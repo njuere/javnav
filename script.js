40c65c54a4fe9fc2c0f262ad7eb9cdae8522a6e7
njuere <njuereosang@gmail.com>
2022-09-23 19:20:49 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let tribox = document.getElementById('tribox')
+let products = document.getElementById('products')
+let menu = document.getElementById('menu')
+let responsive = document.getElementById('responsive')
+let close = document.getElementById('close')
+
+products.addEventListener('click', function(){
+     tribox.style.display = 'block'
+})
+
+menu.addEventListener('click', function (){
+     tribox.style.display = 'block';
+     menu.style.display = 'none';
+     responsive.style.display = 'block';
+     
+})
+
+close.addEventListener('click', function(){
+     tribox.style.display = 'none'
+     menu.style.display = 'block'
+})
+
+
+
+
+
+menu.addEventListener('click', function() {
+     links.style.display = 'block'; 
+     menu.style.display= 'none' ;
+     close.style.display = 'block';   
+ })
+ 
+ close.addEventListener('click', function(){
+     links.style.display = 'none';
+     menu.style.display= 'block' ;
+     close.style.display = 'none';
+ })
+ 
