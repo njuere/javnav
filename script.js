382020e0b918b60b82349b231f23cb715abd188f
njuere <njuereosang@gmail.com>
2022-10-05 17:26:16 +0800

    changes

--- script.js
+++ script.js
@@ -4,20 +4,25 @@ let menu = document.getElementById('menu')
 let responsive = document.getElementById('responsive')
 let close = document.getElementById('close')
 
-products.addEventListener('click', function(){
-     tribox.style.display = 'block'
-})
+products.onclick = function() {
+          if (tribox.style.display == 'none') {
+               tribox.style.display = 'block'
+          } else {
+               tribox.style.display = 'none'
+          }
+     }
 
 menu.addEventListener('click', function (){
      tribox.style.display = 'block';
      menu.style.display = 'none';
      responsive.style.display = 'block';
-     
+     close.style.display = 'block'
 })
 
 close.addEventListener('click', function(){
      tribox.style.display = 'none'
      menu.style.display = 'block'
+     close.style.display = 'none'
 })
 
 
